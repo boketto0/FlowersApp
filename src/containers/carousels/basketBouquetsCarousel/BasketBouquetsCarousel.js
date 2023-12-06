937f1e4826e9934249ab87081aaf0c6153740890
boketto0 <bekmina.julia@gmail.com>
2023-12-06 13:52:24 +0300

    гребаная кнопка

--- src/containers/carousels/basketBouquetsCarousel/BasketBouquetsCarousel.js
+++ src/containers/carousels/basketBouquetsCarousel/BasketBouquetsCarousel.js
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Card, CardType } from '../../../components/cards/Card';
 import { CardWrapper } from '../../../components/cards/CardWrapper';
+import { useSprings, animated } from "react-spring";
+import { useSwipeable } from "react-swipeable";
 import { DetailButton, DetailButtonSize } from '../../../components/detailButton/DetailButton';
 import Icon1 from '../../../assets/productPage/flowers1.jpg'
 import Icon2 from '../../../assets/productPage/flowers2.jpg'
@@ -9,7 +11,12 @@ import Icon4 from '../../../assets/productPage/flowers4.jpg'
 import Icon5 from '../../../assets/productPage/flowers5.jpg'
 import { Button, ButtonSize } from '../../../components/button/Button';
 
+const cardWidth = 10;
+const gap = 2;
+const cardsToScroll = 3;
+
 export const BasketBouquetsCarousel = ( ) => {
+    const index = useRef(0);
 
     const basketElements = [
         {
@@ -48,60 +55,50 @@ export const BasketBouquetsCarousel = ( ) => {
             component:<Button size={ButtonSize.SMALL} colored="white"/>
         },
     ]
-  const [slideIndex, setSlideIndex] = useState(0);
-  const [visibleCards, setVisibleCards] = useState(3);
 
-  const handleSlide = (direction) => {
-    if (direction === 'left') {
-      setSlideIndex((prevSlideIndex) => prevSlideIndex - 1);
-    } else if (direction === 'right') {
-      setSlideIndex((prevSlideIndex) => prevSlideIndex + 1);
-    }
-  };
-
-  useEffect(() => {
-    const updateVisibleCards = () => {
-      const screenWidth = window.innerWidth;
-      if (screenWidth >= 768) {
-        setVisibleCards(5);
-      } else {
-        setVisibleCards(3);
-      }
+    const [props, api] = useSprings(basketElements.length, (i) => ({
+        x: (i - index.current) * (cardWidth + gap),
+        config: { tension: 80, friction: 30 },
+    }));
+    
+    const handlers = useSwipeable({
+        onSwipedLeft: () => handleSwipe(1),
+        onSwipedRight: () => handleSwipe(-1),
+        trackMouse: true,
+    });
+    
+    const handleSwipe = (direction) => {
+        const nextIndex = (index.current + direction * cardsToScroll + basketElements.length) % basketElements.length;
+    
+        if (nextIndex !== 0) {
+            index.current = nextIndex;
+          api.start((i) => ({ x: (i - (index.current - cardsToScroll)) * (cardWidth + gap) }));
+        }
     };
-
-    window.addEventListener('resize', updateVisibleCards);
-    updateVisibleCards();
-
-    return () => {
-      window.removeEventListener('resize', updateVisibleCards);
-    };
-  }, []);
 
     return (
-    <div>
+    <div {...handlers}>
         <div className='product-page__text__wrapper'>
             <h2>Букеты в корзинах</h2>
-            <DetailButton direction={'right'} size={DetailButtonSize.MEDIUM} handleSlide={handleSlide} />
+            <DetailButton direction={'right'} size={DetailButtonSize.MEDIUM}/>
         </div>
-        <div className='product-page__carousel__wrapper'>
-            <DetailButton direction={'left'} size={DetailButtonSize.SMALL} handleSlide={() => handleSlide('left')} />
+        <div>
             <CardWrapper>
-                {basketElements
-                .slice(slideIndex, slideIndex + visibleCards)
-                .map((bel, index) => (
-                <div key={index}>
+            {props.map(({ x }, index) => (
+            <animated.div key={index} style={{ }}>
+                {basketElements[index] && (
                     <Card
                         cardType={CardType.FIRST}
-                        image={bel.image}
-                        price={bel.price}
-                        title={bel.title}
-                        text={bel.text}
-                        component={bel.component}
+                        image={basketElements[index].image}
+                        price={basketElements[index].price}
+                        title={basketElements[index].title}
+                        text={basketElements[index].text}
+                        component={basketElements[index].component}
                     />
-                </div>
-            ))}
+                    )}
+                    </animated.div>
+                    ))}
             </CardWrapper>
-            <DetailButton direction={'right'} size={DetailButtonSize.SMALL} handleSlide={() => handleSlide('right')} />
         </div>
     </div>
     );
